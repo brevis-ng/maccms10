4f2dda2b10a0ec84369e8311ba4b76fffca28c3c
brevis-ng <brevisnguyen@gmail.com>
2022-03-12 17:32:41 +0800

    First commit

---
+++ application/extra/bind.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  '7a4856e7b6a1e1a2580a9b69cdc7233c_5' => 6,
+  '28cd58dff46743fc70710aaa44fa064a_1' => 1,
+  '28cd58dff46743fc70710aaa44fa064a_2' => 2,
+  '28cd58dff46743fc70710aaa44fa064a_11' => 24,
+  '28cd58dff46743fc70710aaa44fa064a_12' => 12,
+  '28cd58dff46743fc70710aaa44fa064a_13' => 13,
+);
